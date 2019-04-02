85ec673a8b5c12792db8eb3a0c1fe1548d4b66ce
James Trafny <jmtrafny@gmail.com>
2019-04-02 12:01:12 -0400

    initial commit with examples

---
+++ ClientServerWithMenuExample/Client/transmission.c
@@ -0,0 +1,22 @@
+#include <sys/socket.h> /* for recv() and send() */
+void DieWithError(char *errorMessage);  /* Error handling function */
+
+void get(int sock, void * buffer, unsigned int size)
+{
+  unsigned int totalBytesRcvd = 0;
+  unsigned int bytesRcvd = 0;
+
+  while(totalBytesRcvd < size)
+  {
+     if((bytesRcvd = recv(sock, buffer, size, 0)) <= 0)
+       DieWithError("recv() failed or connection closed prematurely");
+     totalBytesRcvd += bytesRcvd;
+  }
+}
+
+void put(int sock, void * buffer, unsigned int size)
+{
+    if (send(sock, buffer, size, 0) != size)
+        DieWithError("send() failed");
+}
+
