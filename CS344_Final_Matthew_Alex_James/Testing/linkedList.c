a8b1c3cd3faaba41fd7883fb2aab9cfa30b8ab69
James Trafny <jmtrafny@gmail.com>
2019-04-10 21:15:33 -0400

    experimenting with structs and dlls
    have all files in director, type "make" then "./lltf" to run

---
+++ CS344_Final_Matthew_Alex_James/Testing/linkedList.c
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.h"
+#include "projectStructure.h"
+
+//void append(LINKED_LIST * listPtr, struct * project);
+void append(LINKED_LIST * listPtr, void * data)
+{
+	//int * dataCopy = (int *)malloc(sizeof(int));
+	//dataCopy = data;
+	if (data == NULL) return;
+	/*get a node to put on the list and add the data to it */
+	LIST_NODE * node = (LIST_NODE *) malloc(sizeof(LIST_NODE));
+	node->dataPtr = data;//dataCopy;
+	node->next = NULL;
+	node->prev = NULL;
+
+	listPtr->size++;
+	if(listPtr->front == NULL) /* See if it's the first to go on the list */
+	{
+		listPtr->front = listPtr->back = node;
+		return;
+	}
+	
+	listPtr->back->next = node;
+	node->prev = listPtr->back;
+	listPtr->back = node;
+}
+
+void prepend(LINKED_LIST * listPtr, void * data)
+{
+	if (data == NULL) return;
+	/*get a node to put on the list and add the data to it */
+	LIST_NODE * node = (LIST_NODE *) malloc(sizeof(LIST_NODE));
+	node->dataPtr = data;
+	node->next = NULL;
+	node->prev = NULL;
+
+	listPtr->size++;
+	if(listPtr->front == NULL) /* See if it's the first to go on the list */
+	{
+		listPtr->front = listPtr->back = node;
+		return;
+	}
+	
+	listPtr->front->prev = node;
+	node->next = listPtr->front;
+	listPtr->front = node;
+}
+
+//int insert(LINKED_LIST * listPtr, struct * project, int position);
+int insert(LINKED_LIST * listPtr, void * data, int position){
+	// Setup pointers
+	LIST_NODE * tmpPtr = listPtr->front;
+	LIST_NODE * newNode = (LIST_NODE *) malloc(sizeof(LIST_NODE));
+	newNode->dataPtr = data;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+
+	// Edge cases
+	if (listPtr->size == 0){ return -1; }
+	if (position < 0 || position > listPtr->size){ return -1; }
+	if (position == 0){ 
+		prepend(listPtr, data);
+		//prepend already increases size
+		//listPtr->size++;
+		return 0;
+	}
+	if (position == listPtr->size){ 
+		append(listPtr, data); 
+		//append already increases size
+		//listPtr->size++;
+		return 0;
+	}
+
+	// Traverse to insert location
+	int i; // for loop counter
+	for(i = 0; i < position; i++){
+		tmpPtr = tmpPtr->next;
+	}
+
+	// Insert node
+	newNode->prev = tmpPtr->prev;
+	newNode->next = tmpPtr;
+	tmpPtr->prev->next = newNode;
+	tmpPtr->prev = newNode;
+
+	// Clean up
+	listPtr->size++;
+	//free(tmpPtr);
+	return 0;
+}
+
+/*
+ * Function:  removeFromFront 
+ * --------------------
+ * Takes in a linked list and removes the front node 
+ *
+ *  listPtr: pointer to list to manipulate
+ *
+ *  returns: pointer to data held by deleted node
+ */
+void * removeFromFront(LINKED_LIST * listPtr){
+	LIST_NODE * tmpPtr = listPtr->front;
+	void * tempData = tmpPtr->dataPtr;
+
+	// Edge cases
+	if (listPtr->size == 0) return NULL;
+	if (listPtr->front == listPtr->back) {
+		listPtr->size--;
+		listPtr->front = listPtr->back = NULL;
+		return tempData;
+	}
+	
+	// There are two or greater nodes in list
+	listPtr->front = listPtr->front->next;
+	listPtr->front->prev = NULL;
+	free(tmpPtr);
+	
+	listPtr->size--;
+	return tempData;
+}
+
+/*
+ * Function:  removeFromBack 
+ * --------------------
+ * Takes in a linked list and removes the rear node 
+ *
+ *  listPtr: pointer to list to manipulate
+ *
+ *  returns: pointer to data held by deleted node
+ */
+void * removeFromBack(LINKED_LIST * listPtr){
+	LIST_NODE * tmpPtr = listPtr->back;
+
+	// Edge cases
+	if (listPtr->size == 0) return NULL;
+	if (listPtr->front == listPtr->back) {
+		listPtr->back = NULL;
+	}
+	
+	void * tempData = tmpPtr->dataPtr;
+
+	// There are two or greater nodes in list
+	listPtr->back = listPtr->back->prev;
+	listPtr->back->next = NULL;
+	free(tmpPtr);
+	
+	listPtr->size--;
+	return tempData;
+}
+
+//void * removeAtPosition(LINKED_LIST * listPtr, int position);
+void * removeAtPosition(LINKED_LIST * listPtr, int position){
+	// Setup pointers
+	LIST_NODE * tmpPtr = listPtr->front;
+	void * tempData;
+
+	// Edge cases
+	if (listPtr->size == 0){ return NULL; }
+	if (position < 0 || position >= listPtr->size){ return NULL; }
+	if (position == 0){ 
+		removeFromFront(listPtr);
+		//removeFromFront already reduces size
+		//listPtr->size--;
+		return 0;
+	}
+	if (position == listPtr->size-1){ 
+		removeFromBack(listPtr); 
+		//removeFromFront already reduces size
+		//listPtr->size--;
+		return 0;
+	}
+
+	// Traverse to insert location
+	int i; // for loop counter
+	for(i = 0; i < position; i++){
+		tmpPtr = tmpPtr->next;
+	}
+	tempData = tmpPtr->dataPtr;
+
+	// Remove node
+	tmpPtr->prev->next = tmpPtr->next;
+	tmpPtr->next->prev = tmpPtr->prev;
+
+	// Clean up
+	listPtr->size--;
+	free(tmpPtr);
+	return tempData;
+}
+
+void printProject(void * dataPtr){
+	PROJECT_STRUCT * ps = (PROJECT_STRUCT *) dataPtr;
+	printf("Project ID: %d\n", ps->proj_id);
+	printf("Project Description: %s\n", ps->proj_desc);
+	printf("Date Created: %s\n", ps->proj_date_created);
+	printf("Due Date: %s\n", ps->proj_date_due);
+	printf("Number of Members: %d\n", ps->proj_num_members);
+	printf("Members:\n");
+	traverseForward(ps->proj_member_list, displayMembers);
+}
+
+//void display(struct * project);
+// Author: DBG
+void display(void * data)
+{
+	int * ptr = (int *) data;
+	printf("%d\n",  *ptr);
+}
+
+void displayMembers(void * dataPtr){
+	char * arr = dataPtr;
+	printf("\t%s\n", arr);
+}
+
+void displayProjID(void * dataPtr){
+	PROJECT_STRUCT * ps = (PROJECT_STRUCT *) dataPtr;
+	printf("%d\n", ps->proj_id); 
+}
+
+void displayProjDesc(void * dataPtr){
+	PROJECT_STRUCT * ps = (PROJECT_STRUCT *) dataPtr;
+	printf("%s\n", ps->proj_desc); 
+}
+
+// Author: DBG
+void traverseForward(LINKED_LIST head, void function(void *))
+{
+	LIST_NODE * front = head.front;
+	while(front)
+	{
+		function(front->dataPtr);
+		front = front->next;
+	}
+}
+
+
+// Author: DBG
+void traverseBackward(LINKED_LIST head, void function(void *))
+{
+	LIST_NODE * back = head.back;
+	while(back)
+	{
+		function(back->dataPtr);
+		back = back->prev;
+	}
+}
+
---
+++ CS344_Final_Matthew_Alex_James/Testing/linked_list_to_file.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedList.h"
+#include "projectStructure.h"
+
+int main(int argc, char *argv[])
+{
+	char member1[] = "James";
+	char member2[] = "Stephanie";
+	char member3[] = "Lilly"; 
+	char member4[] = "Olive";
+
+	// Create project linkedlist
+	LINKED_LIST project_list;
+	project_list.size = 0;
+	project_list.front = project_list.back = NULL;
+		// Create member linkedlist for proj 1
+	LINKED_LIST project_1_member_list;
+	project_1_member_list.size = 0;
+	project_1_member_list.front = project_1_member_list.back = NULL;
+		// Create member linkedlist for proj 2
+	LINKED_LIST project_2_member_list;
+	project_2_member_list.size = 0;
+	project_2_member_list.front = project_2_member_list.back = NULL;
+
+	// Create struct for first proj
+	PROJECT_STRUCT * ps1 = (PROJECT_STRUCT *) malloc(sizeof(PROJECT_STRUCT));
+	ps1->proj_id = 1;																// Project ID
+	strncpy(ps1->proj_desc, "Description of project 1.", sizeof(ps1->proj_desc));	// Project Description
+	strncpy(ps1->proj_date_created, "04102019", sizeof(ps1->proj_date_created));	// Creation date
+	strncpy(ps1->proj_date_due, "04252019", sizeof(ps1->proj_date_due));			// Due date
+	append(&project_1_member_list, &member1);
+	append(&project_1_member_list, &member2);
+	ps1->proj_num_members = project_1_member_list.size;								// Number of members
+	ps1->proj_member_list = project_1_member_list;									// Member list
+
+	// Create struct for second proj
+	PROJECT_STRUCT * ps2 = (PROJECT_STRUCT *) malloc(sizeof(PROJECT_STRUCT));
+	ps2->proj_id = 2;
+	strncpy(ps2->proj_desc, "Description of project 2.", sizeof(ps2->proj_desc));
+	strncpy(ps2->proj_date_created, "03092018", sizeof(ps2->proj_date_created));
+	strncpy(ps2->proj_date_due, "06102018", sizeof(ps2->proj_date_due));
+	append(&project_2_member_list, &member3);
+	append(&project_2_member_list, &member4);
+	ps2->proj_num_members = project_2_member_list.size;
+	ps2->proj_member_list = project_2_member_list;
+
+	// Add project structs to project list	
+	append(&project_list, ps1);
+	append(&project_list, ps2);
+	
+	//-----------------------------------------------
+	// Display all project IDs
+	//traverseForward(project_list, displayProjID);
+	//-----------------------------------------------
+	// Display all project Descriptions
+	//traverseForward(project_list, displayProjDesc);
+	//-----------------------------------------------
+	// Display members of each project
+	//printf("Member of project 1: \n");
+	//traverseForward(project_1_member_list, displayMembers);
+	//printf("Member of project 2: \n");
+	//traverseForward(project_2_member_list, displayMembers);
+	//-----------------------------------------------
+	// Display all project info
+	printf("\nPrinting Project 1 info:\n");
+	printProject(ps1);
+	printf("\nPrinting Project 2 info:\n");
+	printProject(ps2);
+	//-----------------------------------------------
+	
+	printf("ok\n");
+	
+	return 0;
+}
