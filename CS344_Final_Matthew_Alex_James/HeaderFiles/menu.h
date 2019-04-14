1278259ba8289bba779c1858d446a774d69582a1
Alex Hall <halla22@my.erau.edu>
2019-04-14 15:04:43 -0400

    menu header file
    Will be used in HandleTCPClientWithMenu.c. Specifically used in the sendMenuAndWaitForResponse() function.

---
+++ CS344_Final_Matthew_Alex_James/HeaderFiles/menu.h
@@ -0,0 +1,16 @@
+
+#ifndef MENU_H
+#define MENU_H
+
+typedef struct menu{
+	unsigned char option1[20];
+	unsigned char option2[20];
+	unsigned char option3[20];
+	unsigned char option4[20];
+	unsigned char option5[20];
+	unsigned char option6[20];
+} MENU;
+
+
+
+#endif //MENU_H
