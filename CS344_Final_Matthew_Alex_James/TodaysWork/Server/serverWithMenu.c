a5a3d67adc757c5479ddc70fb736960094a5c8af
James Trafny <jmtrafny@gmail.com>
2019-04-26 11:58:40 -0400

    new folder cuz im to lazy to branch

---
+++ CS344_Final_Matthew_Alex_James/TodaysWork/Server/serverWithMenu.c
@@ -0,0 +1,90 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+
+#define MAXPENDING 5    /* Maximum outstanding connection requests */
+
+void DieWithError(char *errorMessage);  /* Error handling function */
+void HandleTCPClient(int clntSocket);   /* TCP client handling function */
+
+int main(int argc, char *argv[])
+{
+	pid_t childpid;
+    int servSock;                    /* Socket descriptor for server */
+    int clntSock;                    /* Socket descriptor for client */
+    struct sockaddr_in echoServAddr; /* Local address */
+    struct sockaddr_in echoClntAddr; /* Client address */
+    unsigned short echoServPort;     /* Server port */
+    unsigned int clntLen;            /* Length of client address data structure */
+
+    if (argc != 2)     /* Test for correct number of arguments */
+    {
+        fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
+        exit(1);
+    }
+
+    echoServPort = atoi(argv[1]);  /* First arg:  local port */
+
+    /* Create socket for incoming connections */
+    if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        DieWithError("socket() failed");
+      
+    /* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
+    echoServAddr.sin_family = AF_INET;                /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port = htons(echoServPort);      /* Local port */
+
+    /* Bind to the local address */
+    if (bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+        DieWithError("bind() failed");
+
+    /* Mark the socket so it will listen for incoming connections */
+    if (listen(servSock, MAXPENDING) < 0)
+        DieWithError("listen() failed");
+
+    unsigned int childProcCount = 0;
+    for (;;) /* Run forever */
+    {
+        /* Set the size of the in-out parameter */
+        clntLen = sizeof(echoClntAddr);
+
+        /* Wait for a client to connect */
+        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, 
+                               &clntLen)) < 0)
+            DieWithError("accept() failed");
+
+        /* clntSock is connected to a client! */
+        printf("Passing client to child process: %s\n", inet_ntoa(echoClntAddr.sin_addr));
+
+        childpid = fork();
+        if (childpid == -1) {
+            DieWithError("Failed to fork");
+            return 1;
+        }
+        if (childpid == 0){
+            HandleTCPClient(clntSock);   //child process
+            exit(0);                     //child process terminates
+        }
+
+        printf("With child process: %d\n", childpid);
+        close(clntSock);
+        childProcCount++;
+
+        while(childProcCount){ //Clean up zombies
+            childpid = waitpid((pid_t) -1, NULL, WNOHANG); //non blocking wait
+            if(childpid < 0){
+                DieWithError("waitpid() failed");
+            }
+            else if(childpid == 0){//no zombies to wait on
+                break;
+            }
+            else
+                childProcCount--; //cleaned up after a child
+        }
+    }
+    /* NOT REACHED */
+}
