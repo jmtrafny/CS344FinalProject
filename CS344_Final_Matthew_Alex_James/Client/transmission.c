642c5c47160473fe0e4c9cf1db0efe1187c051dd
James Trafny <jmtrafny@gmail.com>
2019-04-27 10:39:50 -0400

    update comments

--- CS344_Final_Matthew_Alex_James/Client/transmission.c
+++ CS344_Final_Matthew_Alex_James/Client/transmission.c
@@ -4,8 +4,8 @@ Edited: Alex Hall, James Trafny
 Date: 27 April, 2019
 File: transmission.c
 
-This file runs the transmission between the server and client, and keeps track of the bytes sent
-between the two.
+This file runs the transmission between the server and client,
+and keeps track of the bytes sent between the two.
 */
 
 #include <sys/socket.h> /* for recv() and send() */
@@ -14,11 +14,15 @@ void DieWithError(char *errorMessage);  /* Error handling function */
 /*
 Function: get()
 -----------------------------
-This method receives bytes from the sender and tallies the total amount of bytes received.
-It also will terminate if there is an error by calling DieWithError()
+This method receives bytes from the sender 
+and tallies the total amount of bytes received.
+It also will terminate if there is an error by 
+calling DieWithError()
 
-totalBytesRcvd: An unsigned integer that counts the total amount of bytes received
-bytesRcvd: An unsigned integer that counts the amount of bytes received in any single transmission
+totalBytesRcvd: An unsigned integer that counts the total
+                amount of bytes received
+bytesRcvd: An unsigned integer that counts the amount of 
+           bytes received in any single transmission
 
 returns: void
 */
@@ -39,8 +43,8 @@ void get(int sock, void * buffer, unsigned int size)
 /*
 Function: put()
 -----------------------------
-A method that terminates the program if the received buffer is not equal to the size of the
-message.
+A method that terminates the program if the received
+buffer is not equal to the size of the message.
 
 returns: void
 */
--- CS344_Final_Matthew_Alex_James/Server/transmission.c
+++ CS344_Final_Matthew_Alex_James/Server/transmission.c
@@ -1,5 +1,27 @@
+/*
+Authors: Dr. Bethelmy
+Edited: Alex Hall, James Trafny
+Date: 27 April, 2019
+File: transmission.c
+
+This file runs the transmission between the server and client, and keeps track of the bytes sent
+between the two.
+*/
+
 #include <sys/socket.h> /* for recv() and send() */
 void DieWithError(char *errorMessage);  /* Error handling function */
+
+/*
+Function: get()
+-----------------------------
+This method receives bytes from the sender and tallies the total amount of bytes received.
+It also will terminate if there is an error by calling DieWithError()
+
+totalBytesRcvd: An unsigned integer that counts the total amount of bytes received
+bytesRcvd: An unsigned integer that counts the amount of bytes received in any single transmission
+
+returns: void
+*/
 
 void get(int sock, void * buffer, unsigned int size)
 {
@@ -13,6 +35,15 @@ void get(int sock, void * buffer, unsigned int size)
      totalBytesRcvd += bytesRcvd;
   }
 }
+
+/*
+Function: put()
+-----------------------------
+A method that terminates the program if the received buffer is not equal to the size of the
+message.
+
+returns: void
+*/
 
 void put(int sock, void * buffer, unsigned int size)
 {
