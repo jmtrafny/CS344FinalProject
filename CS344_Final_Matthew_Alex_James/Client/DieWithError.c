8791576740e8ef9f015b4ee06c5220fe65f4667e
mprivat1 <47833416+mprivat1@users.noreply.github.com>
2019-04-27 10:32:43 -0400

    Add files via upload
    Comments on all the files in Client

--- CS344_Final_Matthew_Alex_James/Client/DieWithError.c
+++ CS344_Final_Matthew_Alex_James/Client/DieWithError.c
@@ -1,5 +1,21 @@
+/*
+Author: Dr. Bethelmy
+Date: 27 April, 2019
+File: DieWithError.c
+
+This file terminates the program when called with an error message.
+*/
+
 #include <stdio.h>  /* for perror() */
 #include <stdlib.h> /* for exit() */
+
+/*
+Function: DieWithError()
+---------------------------------
+A method that will exit the program and print an error message.
+
+returns: void
+*/
 
 void DieWithError(char *errorMessage)
 {
